5c065342ebcd59e5d7091c72162967c6f9b92e9d
Miguel Ernesto Huicab Jimenez <22470071@campeche.tecnm.mx>
2026-05-27 11:26:45 -0600

    Cambio el idioma de la interfaz de usuario

--- Movies/Models/Movie.cs
+++ Movies/Models/Movie.cs
@@ -5,10 +5,14 @@ namespace Movies.Models
     public class Movie
     {
         public int id { get; set; }
+        [Display(Name = "Titulo")]
         public string? title { get; set; }
         [DataType (DataType.Date)]
+        [Display(Name = "Fecha de liberacion")]
         public  DateTime ReleaseDate { get; set; }
+        [Display(Name = "Genero")]
         public string? Genre { get; set; }
+        [Display(Name = "Precio")]
         public decimal Price { get; set; }
     }
 }
